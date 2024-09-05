3b40d026d92abc1df49be8ac24d36d8f39bfcf53
John_Duarte <zipaquiradigital@gmail.com>
2024-09-04 19:12:09 -0500

    Actualiza contenido en las páginas de about, blog, docs y pricing

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -5,7 +5,7 @@ import DefaultLayout from '@/layouts/default';
 export default function DocsPage() {
   return (
     <DefaultLayout>
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <section className="flex flex-col items-center justify-center gap-4 py-8 mt-24 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
           <h1 className={title()}>documentos</h1>
         </div>
